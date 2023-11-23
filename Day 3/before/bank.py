7d50c1ce3ebd50ab0d1bf811a64d4301a5719f83
TeddyNgonga <tman_tman_tman@yahoo.com>
2023-11-23 17:27:09 +0000

    Day 3 after!

--- Day 3/before/bank.py
+++ Day 3/before/bank.py
@@ -1,47 +1,44 @@
 from dataclasses import dataclass
 from decimal import Decimal
-from stripe_service import StripePaymentService
+from enum import StrEnum, auto
+from typing import Protocol
 
-
+class PaymentService(Protocol):
+    def process_payment(self, amount: Decimal) -> None:
+        ...
+    
+    def process_payout(self, amount: Decimal) -> None:
+        ...
+        
+class AccountType(StrEnum):
+    SAVINGS = auto()
+    CHECKING = auto()
+    
 @dataclass
-class SavingsAccount:
+class Account:
     account_number: str
     balance: Decimal
-
-
-@dataclass
-class CheckingAccount:
-    account_number: str
-    balance: Decimal
-
-
-class BankService:
-    def deposit(
-        self, amount: Decimal, account: SavingsAccount | CheckingAccount
-    ) -> None:
-        if isinstance(account, SavingsAccount):
-            print(f"Depositing {amount} into Savings Account {account.account_number}.")
+    account_type: AccountType
+    
+    def deposit(self, amount: Decimal) -> None:
+        print(
+            f"Depositing {amount} into {self.account_type} Account {self.account_number}."
+        )
+        self.balance += amount
+    
+    def withdraw(self, amount: Decimal) -> None:
+        if (self.balance - amount) > 0:
+            print(
+                f"Withdrawing {amount} from {self.account_type} Account {self.account_number}."
+            )
+            self.balance -= amount
         else:
-            print(
-                f"Depositing {amount} into Checking Account {account.account_number}."
-            )
-        payment_service = StripePaymentService()
-        payment_service.set_api_key("sk_test_1234567890")
-        payment_service.process_payment(amount)
-        account.balance += amount
-
-    def withdraw(
-        self, amount: Decimal, account: SavingsAccount | CheckingAccount
-    ) -> None:
-        if isinstance(account, SavingsAccount):
-            print(
-                f"Withdrawing {amount} from Savings Account {account.account_number}."
-            )
-        else:
-            print(
-                f"Withdrawing {amount} from Checking Account {account.account_number}."
-            )
-        payment_service = StripePaymentService()
-        payment_service.set_api_key("sk_test_1234567890")
-        payment_service.process_payout(amount)
-        account.balance -= amount
+            print(f"Sorry, not enough money in {self.account_type} Account to withdraw {amount}")
+            
+def deposit(amount: Decimal, account: Account, payment_service: PaymentService) -> None:
+    payment_service.process_payment(amount)
+    account.deposit(amount)
+    
+def withdraw(amount: Decimal, account: Account, payment_service: PaymentService) -> None:
+    payment_service.process_payout(amount)
+    account.withdraw(amount)
--- Day 3/before/main.py
+++ Day 3/before/main.py
@@ -1,18 +1,21 @@
 from decimal import Decimal
-from bank import SavingsAccount, CheckingAccount, BankService
+from stripe_service import StripePaymentService
+from bank import Account, AccountType, deposit, withdraw
+
+API_KEY = "sk_test_1234567890"
 
 
 def main() -> None:
-    savings_account = SavingsAccount("SA001", Decimal("1000"))
-    checking_account = CheckingAccount("CA001", Decimal("500"))
+    savings_account = Account("SA001", Decimal("1000"), AccountType.SAVINGS)
+    checking_account = Account("CA001", Decimal("500"), AccountType.CHECKING)
+    payment_service = StripePaymentService()
+    payment_service.set_api_key(API_KEY)
 
-    bank_service = BankService()
+    deposit(Decimal("200"), savings_account, payment_service)
+    deposit(Decimal("300"), checking_account, payment_service)
 
-    bank_service.deposit(Decimal("200"), savings_account)
-    bank_service.deposit(Decimal("300"), checking_account)
-
-    bank_service.withdraw(Decimal("100"), savings_account)
-    bank_service.withdraw(Decimal("200"), checking_account)
+    withdraw(Decimal("100"), savings_account, payment_service)
+    withdraw(Decimal("200"), checking_account, payment_service)
 
     print(f"Savings Account Balance: {savings_account.balance}")
     print(f"Checking Account Balance: {checking_account.balance}")
